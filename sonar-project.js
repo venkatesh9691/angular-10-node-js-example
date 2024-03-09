e5e6661472044316caba72aeba9683c40a05115c
venkatesh9691 <144669446+venkatesh9691@users.noreply.github.com>
2024-03-09 11:08:44 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,13 +3,13 @@ const sonarqubeScanner = require('sonarqube-scanner');
       serverUrl: 'http://16.170.234.56:9000/',
       options : {
       'sonar.sources': '.',
-    'sonar.projectKey' :'venkateshangular'
+    'sonar.projectKey' :'venkateshangular',
     'sonar.projectName': 'venkateshangular',
-    'sonar.projectVersion': '1.0.0'
-    'sonar.projectDescription':'This is Angular based Node js based App'
-    'sonar.sources': www
-    'sonar.login' :"admin"
-    'sonar.password' : "admin"
+    'sonar.projectVersion': '1.0.0',
+    'sonar.projectDescription':'This is Angular based Node js based App',
+    'sonar.sources': www,
+    'sonar.login' :"admin",
+    'sonar.password' : "admin",
     'sonar.langulage' : 'js'
       }
     }, () => {});
