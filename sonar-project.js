36bc235ab7376925d0fb00200a1289443e78c737
venkatesh9691 <144669446+venkatesh9691@users.noreply.github.com>
2024-03-09 10:43:35 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,16 +3,16 @@ const sonarqubeScanner = require('sonarqube-scanner');
       serverUrl: 'http://localhost:9000',
       options : {
       'sonar.sources': '.',
-
-          sonar.projectKey=venkateshangular
-sonar.projectName=venkateshangular
-sonar.projectVersion=1.0.0
-sonar.projectDescription=Static analysis for the AppName
-sonar.sources=www
-sonar.exclusions=**/node_modules/**,**/*.spec.ts,**/dist/**,**/docs/**,**/*.js,**/coverage/**
-sonar.tests=www 
-sonar.test.inclusions=**/*.spec.ts
-sonar.ts.tslint.configPath=tslint.json
+          
+    sonar.projectKey=venkateshangular
+    sonar.projectName=venkateshangular
+    sonar.projectVersion=1.0.0
+    sonar.projectDescription=Static analysis for the AppName
+    sonar.sources=www
+    sonar.exclusions=**/node_modules/**,**/*.spec.ts,**/dist/**,**/docs/**,**/*.js,**/coverage/**
+    sonar.tests=www 
+    sonar.test.inclusions=**/*.spec.ts
+    sonar.ts.tslint.configPath=tslint.json
       'sonar.inclusions' : 'packages/core/src/**' // Entry point of your code
       }
     }, () => {});
