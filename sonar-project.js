d367ccd49f597f6ec2dc6322ced1fa5355be68e9
venkatesh9691 <144669446+venkatesh9691@users.noreply.github.com>
2024-03-09 10:53:00 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,18 +1,15 @@
 const sonarqubeScanner = require('sonarqube-scanner');
     sonarqubeScanner({
-      serverUrl: 'http://localhost:9000',
+      serverUrl: 'http://16.170.234.56:9000/,
       options : {
       'sonar.sources': '.',
-          
-    sonar.projectKey=venkateshangular
-    sonar.projectName=venkateshangular
-    sonar.projectVersion=1.0.0
-    sonar.projectDescription=Static analysis for the AppName
-    sonar.sources=www
-    sonar.exclusions=**/node_modules/**,**/*.spec.ts,**/dist/**,**/docs/**,**/*.js,**/coverage/**
-    sonar.tests=www 
-    sonar.test.inclusions=**/*.spec.ts
-    sonar.ts.tslint.configPath=tslint.json
-      'sonar.inclusions' : 'packages/core/src/**' // Entry point of your code
+        'sonar.projectKey'=venkateshangular
+    'sonar.projectName'=venkateshangular
+    'sonar.projectVersion'=1.0.0
+    'sonar.projectDescription'='This is Angular based Node js based App'
+    'sonar.sources'=www
+    'sonar.login' = "admin"
+    'sonar.password' = "admin"
+    'sonar.langulage' = 'js'
       }
     }, () => {});
